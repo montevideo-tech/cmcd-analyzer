cb662b60b9aa8f98b55935600937e5d582f912e8
adriansqualabs <adrians@qualabs.com>
2023-04-25 15:08:48 -0300

    1-m3-configure-the-cmcd-analizer-project

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
 import { PORT } from "./config.js";
 import app from "./app.js";
 
-
-
-//app.get('/', (req, res) => res.send('Hello World'));
-
 app.listen(PORT);
 console.log(`Server on port http://localhost:${PORT}`);
