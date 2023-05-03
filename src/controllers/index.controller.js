63766e5a9d3a67164dd6ca14801243a7b5d09fe3
mcamardon <martinac@qualabs.com>
2023-05-03 14:26:14 -0300

    4-m3-define-cmcd-request-type: removed commented consol.log.

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -2,7 +2,6 @@ import getCMCDRequestType from "../utils/getCMCDRequestType";
 
 
 export const index = (req, res) => {
-  // console.log(req);
   res.json({ message: "Hello World Controller" });
   getCMCDRequestType(req);
 };
