6ad866fd4093c3d166065955d44ebd7454b68601
Camilo Borba <camilob@qualabs.com>
2023-05-04 15:12:40 -0300

    7-m3-crate-proxy-controller: cors have been added. Try catch has been changed

--- src/app.js
+++ src/app.js
@@ -2,9 +2,11 @@ import express from "express";
 
 import indexRoutes from "./routes/index.routes.js";
 import videoTestRoutes from "./routes/video-test.routes.js"
+import cors from 'cors'
 
 const app = express();
 
+app.use(cors())
 app.use("/", indexRoutes);
 
 app.use("/video-test", videoTestRoutes);
--- src/controllers/video-test.controller.js
+++ src/controllers/video-test.controller.js
@@ -4,12 +4,20 @@ export const videoTest = async (req, res) => {
 
     try {
         const {headers, data} = await videoTestService(req);
-        console.log(headers, data)
         res.header(headers)
         data.pipe(res);
     } catch (error) {
-        console.error(error);
-        res.status(500).send('Internal server error');
+        if (error.response) {
+            // The client was given an error response (5xx, 4xx)
+            console.log(error.response.status, error.response.statusText);
+            res.status(error.response.status).send(error.response.statusText);
+        // } else if (error.request) {
+        //     // The client never received a response, and the request was never left
+        } else {
+            // Anything else
+            res.status(500).send('Internal server error');
+        }
+
     }
 
 };
