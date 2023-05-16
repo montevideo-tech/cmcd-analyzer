b004953adbc8030bdadef58b35c823ab1ac2996d
mcamardon <martinac@qualabs.com>
2023-05-16 11:11:22 -0300

    17-m3-create-video-controller: removed some unecessary code.

--- src/app.js
+++ src/app.js
@@ -2,14 +2,11 @@ import express from "express";
 import videoTestRoutes from "./routes/video-test.routes.js";
 import videoRoutes from "./routes/video.routes.js";
 import cors from 'cors'
-import indexRoutes from "./routes/index.routes.js";
 
 const app = express();
 
 app.use(express.json());
-app.use(cors())
-
-app.use("/", indexRoutes);
+app.use(cors());
 
 app.use("/video-test", videoTestRoutes);
 app.use("/video", videoRoutes);
