977ccdca829ff655ff1906ecabf29d4f2a76d25f
Joaquin Bartaburu <joaquinb@qualabs.com>
2023-05-31 11:06:04 -0300

    feat: transformStream

--- src/services/cmcd-extractor.service.js
+++ src/services/cmcd-extractor.service.js
@@ -1,9 +1,33 @@
 import axios from 'axios';
 import getCMCDRequestType from '../utils/getCMCDRequestType.js'
+import { Transform } from 'stream';
 import { getCMCDParameter } from '../utils/getCMCDParameter.js';
 import { cmcdValidator } from '../utils/cmcdValidator.js';
 import jsLogger from 'js-logger';
 import saveData from '../utils/saveData.js';
+
+
+export const video = async (req, res) => {
+  const response = await axios.get(reqURI, { headers: newHeaders, query: req.query, responseType: 'stream' });
+
+  const transformStream = new Transform({
+    transform(chunk, encoding, callback) {
+      const modifiedChunk = chunk.toString().toUpperCase(); 
+
+      this.push(modifiedChunk);
+
+      callback();
+    }
+  });
+
+  res.set(response.headers);
+
+  response.data.pipe(transformStream);
+
+  transformStream.pipe(res);
+};
+
+
 
 export const cmcdExtractorService = async (req, reqURI, decodedJson, dateStart) => {
     jsLogger.useDefaults({ defaultLevel: jsLogger.TRACE });
@@ -17,9 +41,20 @@ export const cmcdExtractorService = async (req, reqURI, decodedJson, dateStart) 
     const cmcdParam = getCMCDParameter(req, reqURI, type);
     const validatorRes = cmcdValidator(cmcdParam, type, id);
 
-    const {headers, data} = await axios.get(reqURI, { responseType: 'stream', headers:newHeaders, query: req.query });
-    jsLogger.info(`${id}: Saving data into the database...`)
+    const response = await axios.get(reqURI, { responseType: 'stream', headers:newHeaders, query: req.query });
 
+    const transformStream = new Transform({
+        transform(chunk, encoding, callback) {
+
+            const modifiedChunk = chunk.toString().toUpperCase();
+            
+            this.push(modifiedChunk);
+            
+            callback();
+        }
+    });
+    
+    response.data.pipe(transformStream);
     body.id = id;
     body['user-agent'] = req.headers['user-agent'];
     body['request_ip'] = req.headers.origin;
@@ -33,8 +68,9 @@ export const cmcdExtractorService = async (req, reqURI, decodedJson, dateStart) 
     body.warnings = validatorRes.warnings;
     body['cmcd_keys'] = validatorRes.parsedData;
     body['cmcd_data'] = validatorRes.rawData;
-
-    saveData(id, body);
     
-    return {headers: headers, data: data};
+    jsLogger.info(`${id}: Saving data into the database...`)
+    // saveData(id, body);
+    
+    return {headers: response.headers, data: transformStream};
 }
