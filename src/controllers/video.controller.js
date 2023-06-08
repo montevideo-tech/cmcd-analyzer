2a677618f69a97b41e689aa5dbd90906e3bb4815
JoaquinBCh <joaquinb@qualabs.com>
2023-06-07 22:53:21 -0300

    feat: video controller working

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -4,68 +4,78 @@ import { decodeBase64AndConcat } from '../utils/decodeBse64Concat.js'
 import { modifyManifest } from '../utils/modifyManifest.js';
 import { encodeUrl } from '../utils/encodeBse64Concat.js'
 import path from 'path';
-import zlib from 'zlib';
 import log from '../utils/logger.js';
+
+const MAX_PROXY_CACHE_SIZE = 20;
+
+const proxyCache = {
+  cache: {},
+  queue: [],
+  addProxy(target, proxy) {
+    if (this.queue.length >= MAX_PROXY_CACHE_SIZE) {
+      const expiredProxyTarget = this.queue.shift();
+      delete this.cache[expiredProxyTarget];
+    }
+    this.cache[target] = proxy;
+    this.queue.push(target);
+  },
+  getProxy(target) {
+    return this.cache[target];
+  }
+};
 
 export const video = (req, res, next) => {
     
     const dateStart = new Date().toISOString();
     const {id} = req.params;
     try {
-        const ext = path.extname(req.params[0]);
-        const isManifest = ext === '.m3u8' || ext === '.mpd';
-        const jsonBase64 = req.params['jsonbase64'];
+
         const videoURL = req.params[0];
+        const jsonBase64 = req.params['jsonbase64'];
         const {concatenatedUrl, decodedJson}  = decodeBase64AndConcat(jsonBase64, videoURL);
-        const baseUrl = `${req.protocol}://${req.get('host')}/video/${id}/`;
 
-        const proxy = createProxyMiddleware({
-            target: decodedJson.url,
-            changeOrigin: true,
-            selfHandleResponse: isManifest,
-            pathRewrite: function (path, req) {
+        let proxy = proxyCache.getProxy(decodedJson.url);
+        if (!proxy) {
+            proxy = createProxyMiddleware({
+                target: decodedJson.url,
+                changeOrigin: true,
+                selfHandleResponse: true,
+                pathRewrite: (path, req) => {
 
-                const resPath = path.replace(`/video/${req.params.id}/${req.params.jsonbase64}`, '');
+                    const resPath = path.replace(`/video/${req.params.id}/${req.params.jsonbase64}`, '');
 
-                return resPath;
-            },
-            onProxyRes: responseInterceptor(async (responseBuffer, proxyRes, req, res) => {
+                    return resPath;
+                },
+                onProxyRes: responseInterceptor(async (responseBuffer, proxyRes, req, res) => {
+                    const statusCode = proxyRes.statusCode;
+
+                    const baseUrl = `${req.protocol}://${req.get('host')}/video/${id}/`;
+                    const ext = path.extname(req.params[0]);
+                    const isManifest = ext === '.m3u8' || ext === '.mpd';
+                    const jsonBase64 = req.params['jsonbase64'];
+                    const videoURL = req.params[0];
+                    const {concatenatedUrl, decodedJson} = decodeBase64AndConcat(jsonBase64, videoURL);
                     
-                switch (proxyRes.statusCode) {
-                    case 200:
-                        if (isManifest) {
-                            let manifest = '';
-                            manifest = responseBuffer.toString();                            
-                            manifest = modifyManifest(concatenatedUrl, manifest, baseUrl, decodedJson);
-                            return manifest;
-                        } else {
-                            return responseBuffer;
-                        }
-                            
-                    // eslint-disable-next-line no-unreachable
-                    break;
-
-                    case 302:
-                        // eslint-disable-next-line no-case-declarations
-                        const newLocation = encodeUrl(proxyRes.headers['location'], baseUrl, decodedJson);
+                    if (statusCode === 200 && isManifest) {
+                        let manifest = '';
+                        manifest = responseBuffer.toString();                            
+                        manifest = modifyManifest(concatenatedUrl, manifest, baseUrl, decodedJson);
+                        return manifest;  
+                    } else if (statusCode === 301 || statusCode === 302) {
+                        const { concatenatedUrl } = encodeUrl(proxyRes.headers['location'], baseUrl, decodedJson);
 
                         res.statusCode = proxyRes.statusCode;
-                        res.setHeader('location', newLocation.concatenatedUrl);
+                        res.setHeader('location', concatenatedUrl);
                         return responseBuffer;
-                    // eslint-disable-next-line no-unreachable
-                    break;
-                    case 301:
-                    // ...
-                    break;
-
-                    default:
+                    } else {
                         res.statusCode = proxyRes.statusCode;
-                        return responseBuffer; 
-                }
-                
-            }),
-
-        });
+                        res.headers = proxyRes.headers;
+                        return responseBuffer;
+                    }
+                }),
+            });
+            proxyCache.addProxy(decodedJson.url, proxy);
+        }
 
         proxy(req, res, next);
         // cmcdExtractorService(id, req, concatenatedUrl, decodedJson, dateStart)
