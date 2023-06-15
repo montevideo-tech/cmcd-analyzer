a7685201a61bca9426ca93930892893886c88e05
mcamardon <martinac@qualabs.com>
2023-06-15 11:30:55 -0300

    deleted unnecessary folder

--- analyzer-dashboard/src/components/Button/URLGenerateButton.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import '../../App.css'
-
-function URLGenerateButton({ onClick, name }) {
-  return <button className="custom-button" onClick={onClick}>{name}</button>;
-}
-
-export default URLGenerateButton;
