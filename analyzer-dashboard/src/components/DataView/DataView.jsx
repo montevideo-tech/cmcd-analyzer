32c014191543f27b4ccd5cf36370bee6641b3dc6
JoaquinBCh <joaquinb@qualabs.com>
2023-06-30 11:10:22 -0300

    feat: DataView was prittified

--- analyzer-dashboard/src/components/DataView/DataView.jsx
+++ analyzer-dashboard/src/components/DataView/DataView.jsx
@@ -6,6 +6,36 @@ const DataView = ({show, setShow, data}) => {
 
   const handleClose = () => setShow(false);
 
+  const renderValue = (value, prefix = '') => {
+    console.log(value);
+    if (typeof value === 'boolean') {
+      return value ? 'true' : 'false';
+    }
+    if (typeof value === 'object') {
+      return renderKeyValuePairs(value, prefix);
+    }
+    return value;
+  };
+
+  const renderKeyValuePairs = (obj, prefix = '') => {
+    return Object.entries(obj).map(([key, value]) => {
+      const fullKey = prefix ? `${prefix}.${key}` : key;
+      return (
+        <div key={fullKey} className='mb-2'>
+          <strong>{fullKey}</strong>
+          <div className='mx-3 data-value'>{renderValue(value, fullKey)}</div>
+        </div>
+      );
+    });
+  };
+
+  const renderTopLevel = () => {
+    if (data === null) {
+      return <div>No data available.</div>;
+    }
+    return renderKeyValuePairs(data);
+  };
+
   return (
     <div>
       <Offcanvas show={show} onHide={handleClose} scroll={true} backdrop={false}>
@@ -15,7 +45,10 @@ const DataView = ({show, setShow, data}) => {
           </Offcanvas.Title>
         </Offcanvas.Header>
         <Offcanvas.Body>
-        <pre>{JSON.stringify(data, null, 2)}</pre>
+          <div>
+            {renderTopLevel(data)}
+          </div>
+        {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
         </Offcanvas.Body>
       </Offcanvas>
     </div>
